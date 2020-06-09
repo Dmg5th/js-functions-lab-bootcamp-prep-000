1b80c2cf8737c80c615e537ae8a29ac66f7f3b1c
Daniel Gould <dmgould310@gmail.com>
2020-06-09 13:53:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ function happyHolidaysTo(name){
 function happyCustomHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown (holiday, )
+function holidayCountdown (holiday, days){
+  return `
+}
 
